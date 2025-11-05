b84ec52334b351a01e52a225dca16c55f846c905
ljs981026 <ljs981026@naver.com>
2025-11-05 18:07:43 +0900

    init

---
+++ src/main/java/io/devground/dbay/DbayApplication.java
@@ -0,0 +1,13 @@
+package io.devground.dbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DbayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/devground/dbay/DbayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devground.dbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
